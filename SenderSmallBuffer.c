b1881c04d4e6a73e4915fc6128f4a46743e5e6c4
Shavindra Fernando <shavindrag13@gmail.com>
2025-06-02 09:46:55 +0530

    Sender and receiver programs using message queue with small buffer size

---
+++ SenderSmallBuffer.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#define MAX 10
+
+// Structure for message queue
+struct mesg_buffer {
+    long mesg_type;
+    char mesg_text[100];
+} message;
+
+int main() {
+    key_t key;
+    int msgid;
+
+    // Generate unique key
+    key = ftok("progfile", 65);
+
+    // Create message queue
+    msgid = msgget(key, 0666 | IPC_CREAT);
+    message.mesg_type = 1;
+
+    printf("Write Data1: ");
+    fgets(message.mesg_text, MAX, stdin);
+
+    // Send message
+    msgsnd(msgid, &message, sizeof(message), 0);
+    printf("Data sent is: %s\n", message.mesg_text);
+
+    return 0;
+}
